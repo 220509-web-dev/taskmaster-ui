dff5aac331e3bfc81da8c1dee33f38ca8905c625
Wezley Singleton <wezley.singleton@gmail.com>
2022-07-06 01:11:33 -0400

    converted Login to use MUI and similar structure as Register

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,7 @@
+import { css } from "@emotion/css";
+import { Button, FormControl, Input, InputLabel, Typography } from "@mui/material";
 import { SyntheticEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import { User } from "../models/user";
 import ErrorMessage from "./ErrorMessage";
@@ -8,36 +11,40 @@ interface ILoginProps {
     setCurrentUser: (nextUser: User) => void
 }
 
+type LoginForm = {
+    username: string;
+    password: string;
+}
+
 function Login(props: ILoginProps) {
 
-    // destructuring assignment
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMsg, setErrorMsg] = useState<string>(); // TS provides generics
+    const navigate = useNavigate();
 
-    let updateUsername = (e: SyntheticEvent) => {
-        // YOU CANNOT UPDATE PIECES OF STATE DIRECTLY BECAUSE STATE IS IMMUTABLE! USE THE SETTER PROVIDED BY useState!
-        // username = (e.target as HTMLInputElement).value; 
-        setUsername((e.target as HTMLInputElement).value);
-    }
-
-    let updatePassword = (e: SyntheticEvent) => {
-        setPassword((e.target as HTMLInputElement).value);
+    const [formData, setFormData] = useState<LoginForm>({
+        username: '',
+        password: ''
+    });
+    
+    const [errorMsg, setErrorMsg] = useState<string>();
+    
+    let handleChange = (e: any) => {
+        const { name, value } = e.target;
+        setFormData({...formData, [name]: value});
     }
 
     let login = async (e: SyntheticEvent) => {
-
-        // Prevent whatever default event logic would run otherwise (we only want our logic to run)
-        e.preventDefault();
         
+        const { username, password } = formData;
+
         if (!username || !password) {
             setErrorMsg('You must provide a username and a password!');
+            return;
         } else {
             setErrorMsg('');
         }
         
         try {
-            let resp = await fetch('http://taskmasterapi-env.eba-6rxd6zer.us-east-1.elasticbeanstalk.com/taskmaster/auth', {
+            let resp = await fetch(`${process.env.REACT_APP_API_URL}/auth`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -57,23 +64,78 @@ function Login(props: ILoginProps) {
     }
 
     return (
+
         props.currentUser ? <Navigate to="/dashboard"/> : 
-        <>
-            <h4>Log into Taskmaster!</h4>
-            <div id="login-form">
-                <input type="text" id="login-username" placeholder="Enter your username" onChange={updateUsername}/>
-                <br/><br/>
-                <input type="password" id="login-password" placeholder="Enter your password" onChange={updatePassword}/>
-                <br/><br/>
-                <button id="login-button" onClick={login}>Login</button>
-                <br/><br/>
+
+        <div 
+            id="login-component"
+            className={css`
+                justify-content: center;
+                margin-left: 32rem;
+                margin-top: 6rem;
+                padding: 1rem;
+                width: 40%;`}>
+
+            <Typography align="center" variant="h4">Log into your account</Typography>
+        
+            <div
+                id="login-form-fields"
+                className={css`padding: .5rem;`}>
+
+                <FormControl margin="normal" fullWidth>
+                    <InputLabel htmlFor="username">Username</InputLabel>
+                    <Input
+                        onChange={handleChange}
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Enter your username"
+                    />
+                </FormControl>
+
+                <FormControl margin="normal" fullWidth>
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <Input
+                        onChange={handleChange}
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Enter your password"
+                    />
+                </FormControl>
             </div>
-            { errorMsg ? 
-                <ErrorMessage errorMessage={errorMsg}/>
-                :
-                <></>    
-            }
-        </>
+
+            <div
+                id="options-container"
+                className={css`margin: .25rem;`}>
+
+                <Button
+                    id="nav-to-register-button"
+                    className={css`float: left;`}
+                    onClick={() => navigate('/register')}
+                    variant="contained"
+                    color="secondary"
+                    size="medium">Register</Button>
+
+                <Button
+                    id="login-button"
+                    className={css`float: right;`}
+                    onClick={login}
+                    variant="contained"
+                    color="primary"
+                    size="medium">Login</Button>
+            
+            </div>
+
+            <div
+                id="error-container"
+                className={css`
+                    margin-top: 4rem;`}>
+                { errorMsg ? <ErrorMessage errorMessage={errorMsg}/> : <></> }
+            </div>
+            
+
+        </div>
     );
 
 }
